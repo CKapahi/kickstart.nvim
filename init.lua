e750c1c702e511bf726bf4b4ee99b3d805f83e3a
Connor Kapahi <connorkapahi@gmail.com>
2026-07-27 18:51:56 -0400

    feat: created auto :Neotree when starting "nvim" without file arg

--- init.lua
+++ init.lua
@@ -968,11 +968,18 @@ do
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug'
+  require 'kickstart.plugins.debug'
   -- require 'kickstart.plugins.indent_line'
   -- require 'kickstart.plugins.lint'
-  -- require 'kickstart.plugins.autopairs'
+  require 'kickstart.plugins.autopairs'
   require 'kickstart.plugins.neo-tree'
+  vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+      if vim.fn.argc() == 0 then
+        vim.cmd("Neotree show")
+      end
+    end,
+  })
   -- require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
 
   -- NOTE: You can add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
